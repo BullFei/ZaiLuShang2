d5cda1a1e2a0bf4a1854c38f295b6bfd09132957
gaocaixin <767374741@qq.com>
2015-05-14 21:57:57 +0800

    底部bar

---
+++ ZaiLuShang2/ZaiLuShangClass/HotTopic(热门)/view/CXScrollViewBarBtn.h
@@ -0,0 +1,15 @@
+//
+//  CXScrollViewBarBtn.h
+//  ZaiLuShang2
+//
+//  Created by gaocaixin on 15/5/14.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXScrollViewBarBtn : UIButton
+
+@property (nonatomic, assign) BOOL isLoading;
+
+@end
