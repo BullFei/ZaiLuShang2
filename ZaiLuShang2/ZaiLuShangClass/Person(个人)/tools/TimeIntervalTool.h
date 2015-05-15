2dce1ec58341f993cd4c557b59ab11b5aba0bfe7
cyruschang <qianfeng@chang.local>
2015-05-15 20:17:28 +0800

    cell定制继续进行中

---
+++ ZaiLuShang2/ZaiLuShangClass/Person(个人)/tools/TimeIntervalTool.h
@@ -0,0 +1,15 @@
+//
+//  TimeIntervalTool.h
+//  ZaiLuShang2
+//
+//  Created by ChangLuyang on 15/5/15.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeIntervalTool : NSObject
+
++ (NSString *)timeIntervalFromTimeString:(NSString *)timeString;
+
+@end
--- ZaiLuShang2/ZaiLuShangClass/Person(个人)/view/TripCell.h
+++ ZaiLuShang2/ZaiLuShangClass/Person(个人)/view/TripCell.h
@@ -9,6 +9,7 @@
 #import "RootTableViewCell.h"
 #import "STTweetLabel.h"
 #import "LYAttention.h"
+#import "LYLCButton.h"
 
 @protocol TripCellDelegate <NSObject>
 
@@ -38,11 +39,19 @@
 // 正文内容
 @property (nonatomic, strong) UILabel *contentLabel;
 // 喜欢按钮
-@property (nonatomic, strong) UIButton *likeButton;
+@property (nonatomic, strong) LYLCButton *likeButton;
 // 评论按钮
-@property (nonatomic, strong) UIButton *commentButton;
-// 创建事件
+@property (nonatomic, strong) LYLCButton *commentButton;
+// 创建时间
 @property (nonatomic, strong) UILabel *createAtLabel;
+
+/*******************************************************/
+// 展示两条评论
+@property (nonatomic, strong) UIImageView *commentatorIcon1;
+@property (nonatomic, strong) UIImageView *commentatorIcon2;
+
+@property (nonatomic, strong) UILabel *commentContent1;
+@property (nonatomic, strong) UILabel *commentContent2;
 
 
 - (instancetype)initWithLYAttention:(LYAttention *)attention;
