b2511b087a5f8c85a0521ead8797d68696a79996
gaocaixin <767374741@qq.com>
2015-05-18 11:04:58 +0800

    1

--- ZaiLuShang2/ZaiLuShangClass/Search(搜索)/controller/SFPhotoVC.m
+++ ZaiLuShang2/ZaiLuShangClass/Search(搜索)/controller/SFPhotoVC.m
@@ -12,8 +12,14 @@
 -(void)viewDidLoad
 {
     [super viewDidLoad];
-    self.otherUrl =[NSString stringWithFormat:@"http://app6.117go.com/demo27/php/discoverAction.php?submit=getLocalityRecords&locid=%ld&length=21&vc=anzhuo&vd=a1c9d9b8a69b4bf4&token=5aa634ad2fd021650587afa999fdd184&v=a6.1.0",self.ID];
-    
+    self.otherUrl =[NSString stringWithFormat:@"http://app6.117go.com/demo27/php/discoverAction.php?submit=getLocalityRecords&locid=%d&length=21&vc=anzhuo&vd=a1c9d9b8a69b4bf4&token=5aa634ad2fd021650587afa999fdd184&v=a6.1.0",self.ID];
+
+}
+
+
+- (void)dealloc
+{
+    [self.header free];
 }
 
 @end
